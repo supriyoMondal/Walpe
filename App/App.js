c790f30c1820ca4e0606c3463acdeba66a692361
Supriyo Mondal <hisupriyooo@gmail.com>
2020-06-26 11:40:27 +0530

    Initial Commit

---
+++ App/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+// https://pixabay.com/api/?key=13641585-020cc5f4af8cd479d68bb896b&image_type=photo&orientation=vertical
+
+const App = () => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>hello from walpe</Text>
+        </View>
+    )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
