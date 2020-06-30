4b448b0ca62b508d3169c65288f62cc2469c61ed
Supriyo Mondal <hisupriyooo@gmail.com>
2020-06-30 11:29:00 +0530

    Category Page

--- App/assets/images.js
+++ App/assets/images.js
@@ -1,3 +1,42 @@
 export const images = {
     fire: require('./images/fire.png')
 }
+
+export const colorsImage = {
+    grayscale: require('./images/grayscale.jpg'),
+    red: require('./images/red.jpg'),
+    black: require('./images/black.jpg'),
+    blue: require('./images/blue.jpg'),
+    brown: require('./images/brown.jpg'),
+    gray: require('./images/gray.jpg'),
+    green: require('./images/green.jpg'),
+    lilac: require('./images/lilac.jpg'),
+    orange: require('./images/orange.jpg'),
+    pink: require('./images/pink.jpg'),
+    turquoise: require('./images/turquoise.jpg'),
+    white: require('./images/white.jpg'),
+    yellow: require('./images/yellow.jpg'),
+}
+
+export const categories = {
+    backgrounds: require('./images/background.jpg'),
+    fashion: require('./images/fashion.jpg'),
+    nature: require('./images/nature.jpg'),
+    science: require('./images/science.jpg'),
+    education: require('./images/education.jpg'),
+    feelings: require('./images/feelings.jpg'),
+    health: require('./images/health.jpg'),
+    people: require('./images/people.jpg'),
+    religion: require('./images/religion.jpg'),
+    places: require('./images/places.jpg'),
+    animals: require('./images/animals.jpg'),
+    industry: require('./images/industry.jpg'),
+    computer: require('./images/computer.jpg'),
+    food: require('./images/food.jpg'),
+    sports: require('./images/sports.jpg'),
+    transportation: require('./images/transportation.jpg'),
+    travel: require('./images/travel.jpg'),
+    buildings: require('./images/buildings.jpg'),
+    business: require('./images/business.jpg'),
+    music: require('./images/music.jpg')
+}
--- App/screens/categories/Index.js
+++ App/screens/categories/Index.js
@@ -1,14 +1,69 @@
 import React, { Fragment } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image, Dimensions, FlatList } from 'react-native'
 import { useTheme } from '@react-navigation/native'
+import { colorsImage, categories } from '../../assets/images';
 
+const { width, height } = Dimensions.get('screen');
 
 const Categories = ({ navigation }) => {
     const { colors } = useTheme();
+
+
+    const allColors = [
+        "grayscale", "red", "orange", "yellow", "green", "turquoise", "blue", "lilac", "pink", "white", "gray", "black", "brown"
+    ]
+    const allCategories = [
+        'backgrounds', 'fashion', 'nature', 'science', 'education', 'feelings', 'health', 'people', 'religion', 'places', 'animals', 'industry', 'computer', 'food', 'sports', 'transportation', 'travel', 'buildings', 'business', 'music'
+    ]
+
+    const CategoryListItem = ({ item }) => {
+        return (
+            <TouchableOpacity
+                activeOpacity={0.8}
+                style={styles.categoryItemContainer}>
+                <Image
+                    style={styles.categoryImage}
+                    source={categories[item]}
+                />
+                <Text style={styles.categoryText}>
+                    {item.toUpperCase()}
+                </Text>
+
+            </TouchableOpacity>
+        )
+    }
+
     return (
         <Fragment>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: colors.dark }}>
-                <Text style={{ color: "#fff" }}>Categories</Text>
+            <View style={{ backgroundColor: colors.dark, paddingBottom: 280 }}>
+                <View >
+                    <Text style={styles.subHeading}>Colors</Text>
+                    <ScrollView horizontal={true}
+                        showsHorizontalScrollIndicator={false}
+                        style={styles.colorScrollbar}>
+                        {allColors.map((c, index) => (
+                            <TouchableOpacity
+                                key={index}
+                                activeOpacity={0.8}
+                                style={{ width: 73, height: 85 }}>
+                                <Image source={colorsImage[c]}
+                                    style={styles.colorImage} />
+                            </TouchableOpacity>
+                        ))}
+
+                    </ScrollView>
+                    <Text style={{ ...styles.subHeading }}>Categories</Text>
+                    <FlatList
+                        style={{
+                            margin: 10,
+                            paddingBottom: 10
+                        }}
+                        data={allCategories}
+                        keyExtractor={(item, index) => index}
+                        numColumns={2}
+                        renderItem={({ item }) => <CategoryListItem item={item} />}
+                    />
+                </View>
             </View>
         </Fragment>
     )
@@ -16,4 +71,13 @@ const Categories = ({ navigation }) => {
 
 export default Categories
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    subHeading: { color: '#ccc', marginLeft: 15, fontSize: 16, marginTop: 5 },
+    colorScrollbar: { marginLeft: 10, marginTop: 7, marginRight: 5 },
+    colorImage: { width: 65, height: 65, borderRadius: 100 },
+    categoryItemContainer: { width: width / 2 - 18, height: 75, marginVertical: 4, marginHorizontal: 4, position: 'relative' },
+    categoryImage: { width: width / 2 - 18, height: 75, opacity: .6 },
+    categoryText: {
+        color: 'floralwhite', fontFamily: 'Lato-Regular', fontSize: 17, alignSelf: 'center', position: 'absolute', top: '40%'
+    }
+})
