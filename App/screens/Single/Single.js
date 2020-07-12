34b03192c0b726917b7d28c105c5945fd5034135
Supriyo Mondal <hisupriyooo@gmail.com>
2020-07-12 11:01:11 +0530

    Full screen

--- App/screens/Single/Single.js
+++ App/screens/Single/Single.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect, Fragment } from 'react'
-import { StyleSheet, Text, View, ActivityIndicator, StatusBar, Image } from 'react-native'
+import { StyleSheet, Text, View, ActivityIndicator, StatusBar, Image, TouchableOpacity, Dimensions } from 'react-native'
 import { colorsFromUrl } from 'react-native-dominant-color'
 import LinearGradient from 'react-native-linear-gradient';
 import { colors } from '../../assets/colors';
+import { Icon } from 'native-base';
 
 
 const Single = ({ navigation, route: { params: { item } } }) => {
     const { largeImageURL, webformatURL } = item;
     const [colorPalate, setColorPalate] = useState(null);
+    const [isFullScreen, setFullScreen] = useState(false);
     const getColorPalate = () => {
         try {
             colorsFromUrl(webformatURL, (err, palate) => {
@@ -24,6 +26,7 @@ const Single = ({ navigation, route: { params: { item } } }) => {
     }
     useEffect(() => {
         getColorPalate();
+        return setFullScreen(false);
     }, [])
 
     if (!colorPalate) {
@@ -36,8 +39,11 @@ const Single = ({ navigation, route: { params: { item } } }) => {
     return (
         <Fragment>
             <StatusBar backgroundColor={colorPalate.light} />
-            <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 0, y: 1 }} colors={[colorPalate.light, colorPalate.average]} style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <View style={{
+            <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 0, y: 1 }} colors={[colorPalate.light, colorPalate.average]} style={{ flex: 1, justifyContent: 'center', alignItems: 'center', position: 'relative' }}>
+                {isFullScreen ? <Image
+                    source={{ uri: largeImageURL }}
+                    style={{ position: 'absolute', top: 0, right: 0, left: 0, bottom: 0 }}
+                /> : <View style={{
                     shadowColor: "#000",
                     shadowOffset: {
                         width: 0,
@@ -46,18 +52,49 @@ const Single = ({ navigation, route: { params: { item } } }) => {
                     shadowOpacity: 0.50,
                     shadowRadius: 12.35,
                     elevation: 19,
-                    backgroundColor: 'rgba(0,0,0,.5)',
+                    backgroundColor: 'rgba(255,255,255,.2)',
                     width: '77%', height: "80%",
-                    borderRadius: 13
+                    borderRadius: 13,
+                    overflow: 'hidden'
                 }}>
-                    <Image
-                        source={{ uri: largeImageURL }}
-                        style={{
-                            width: '100%', height: '100%',
-                            borderRadius: 13
-                        }}
+                        <LinearGradient
+                            start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }} colors={[colorPalate.light, colorPalate.average]}
+                            style={{
+                                width: '100%', height: '100%'
+                            }}>
+                            <TouchableOpacity
+                                activeOpacity={0.9}
+                            >
+                                <Image
+                                    source={{ uri: largeImageURL }}
+                                    style={{
+                                        width: '100%', height: '100%',
+                                        borderRadius: 13
+                                    }}
+                                />
+                            </TouchableOpacity>
+                        </LinearGradient>
+
+                    </View>
+                }
+                <View style={{ flexDirection: 'row', position: 'absolute', bottom: 30 }}>
+                    <Icon name="heart" type='Entypo'
+                        style={{ marginHorizontal: 15, color: colors.darkLight }}
+                    />
+                    {isFullScreen ?
+                        <Icon name="eye-with-line" type="Entypo"
+                            style={{ marginHorizontal: 15, color: colors.darkLight }}
+                            onPress={() => setFullScreen(false)}
+                        /> :
+                        <Icon name="eye" type="Entypo"
+                            style={{ marginHorizontal: 15, color: colors.darkLight }}
+                            onPress={() => setFullScreen(true)}
+                        />}
+                    <Icon name="cellphone-screenshot" type='MaterialCommunityIcons'
+                        style={{ marginHorizontal: 15, color: colors.darkLight }}
                     />
                 </View>
+
             </LinearGradient>
         </Fragment>
     )
---
+++ App/screens/fullscreen/FullScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const FullScreen = () => {
+    return (
+        <View>
+            <Text>FullScreen</Text>
+        </View>
+    )
+}
+
+export default FullScreen
+
+const styles = StyleSheet.create({})
